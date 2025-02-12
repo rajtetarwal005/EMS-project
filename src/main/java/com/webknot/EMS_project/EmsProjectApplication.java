b821cb57066c349681f8dcbd823fbd5609f09073
tetarwal005 <tetarwal005@gmail.com>
2025-02-12 09:51:33 +0530

    Initial commit

---
+++ src/main/java/com/webknot/EMS_project/EmsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.webknot.EMS_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webknot/EMS_project/EmsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webknot.EMS_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
